9555c14e1397424c2858792390febc79aa1a32e9
Emilio López <elopez93@gmail.com>
2019-06-29 20:26:34 -0300

    Fix erroneous tests, add colors

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -4,6 +4,19 @@ SIM_FOLDER="$1"
 TEST_FOLDER="$2"
 SIMPLE="$3"
 
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+
+BASEDIR="$(dirname "$(readlink -f "$0")")"
+
+function run_tiger () {
+    DIR="$PWD"
+    cd "$BASEDIR/../src/"
+    ./tiger "$@"
+    cd "$DIR"
+}
+
 if ! [ "$#" -gt 1 ] ||  ! [ -d "$2" ] ; then
   echo "Usage: $0 SIMULATOR_PATH TEST_FOLDER [-simple]" >&2
   exit 1
@@ -16,20 +29,22 @@ fi
 echo "Running all tests..."
 
 for file in "$TEST_FOLDER"/*; do
+  file=$(readlink -f "$file")
+  name=$(basename "$file")
   if [[ $file == *.tig ]]; then
-    printf "$file ---> "
+    printf "$name ---> "
     LOG_FILE="log.txt"
     OUT_EXT=".output"
     OUTPUT="$file$OUT_EXT"
     EXP_EXT=".expected"
     EXPECTED="$file$EXP_EXT"
     if [ "$SIMPLE" == 0 ]; then
-      ../src/tiger "$file" > "$LOG_FILE"
+      run_tiger "$file" > "$LOG_FILE"
     else
-      ../src/tiger "$file" -simple > "$LOG_FILE"
+      run_tiger "$file" -simple > "$LOG_FILE"
     fi
 
-    script -c "$SIM_FOLDER a.out" "$OUTPUT" > log.tmp
+    script -c "$SIM_FOLDER $BASEDIR/../src/a.out" "$OUTPUT" > log.tmp
     sed -i '1d;$d' "$OUTPUT"
     sed -i '$d' "$OUTPUT"
 
@@ -38,10 +53,10 @@ for file in "$TEST_FOLDER"/*; do
 
     # compare
     DIFF=$(diff "$OUTPUT" "$EXPECTED")
-    if [ "$DIFF" == "" ]; then
-      echo "passed!"
+    if [ "$?" = "0" ] && [ "$DIFF" == "" ]; then
+      echo "${green}passed!${reset}"
     else
-      echo "failed!"
+      echo "${red}failed!${reset}"
     fi
   fi
 done
