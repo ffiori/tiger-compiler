93fcf4bdd3c2ec45445ed8944bc5d46882e2e935
Fernando Fiori <fiorifj@gmail.com>
2019-06-29 17:09:34 -0300

    New tester. Need to rename outputs to expected

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 
-TEST_FOLDER="$1"
-VERBOSITY="$2"
+SIM_FOLDER="$1"
+TEST_FOLDER="$2"
+SIMPLE="$3"
 
-if ! [ "$#" -gt 0 ] || ! [ -d "$1" ] ; then
-  echo "Usage: $0 DIRECTORY [VERBOSITY]" >&2
+if ! [ "$#" -gt 1 ] ||  ! [ -d "$2" ] ; then
+  echo "Usage: $0 SIM_FOLDER TEST_FOLDER [-simple]" >&2
   exit 1
 fi
 
-if ! [ "$#" -gt 1 ]; then
-VERBOSITY=1
+if [ "$#" -lt 3 ]; then
+SIMPLE=0
 fi
 
 echo "Running all tests..."
 
 for file in "$TEST_FOLDER"/*; do
-  if [[ $file == *.txt ]]; then
-	  printf "$file ---> "
-	  ANS=$(../src/tiger "$file" | tail -$VERBOSITY)
-	  echo ${ANS}  
+  if [[ $file == *.tig ]]; then
+    printf "$file ---> "
+    LOG_FILE="log.txt"
+    OUT_EXT=".output"
+    OUTPUT="$file$OUT_EXT"
+    EXP_EXT=".expected"
+    EXPECTED="$file$EXP_EXT"
+    if [ "$SIMPLE" == 0 ]; then
+      ../src/tiger "$file" > "$LOG_FILE"
+    else
+      ../src/tiger "$file" -simple > "$LOG_FILE"
+    fi
+
+    script -c "$SIM_FOLDER a.out" "$OUTPUT" > log.tmp
+    sed -i '1d;$d' "$OUTPUT"
+    sed -i '$d' "$OUTPUT"
+
+    # head -n -2 "$OUTPUT" > "$OUTPUT"
+    # tail -n +2 "$OUTPUT" > "$OUTPUT"
+
+    # compare
+    DIFF=$(diff "$OUTPUT" "$EXPECTED")
+    if [ "$DIFF" == "" ]; then
+      echo "passed!"
+    else
+      echo "failed!"
+    fi
   fi
 done
-
 
