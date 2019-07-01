7dbad838d538d1b0f26ec796712f2d0eb452d3fe
Emilio López <elopez93@gmail.com>
2019-07-01 03:46:24 -0300

    Clean up C runtime

--- src/runtime.c
+++ src/runtime.c
@@ -1,239 +1,294 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <inttypes.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
-void printInt(uint64_t x) {
-	printf("INT: %ld\n", x);
-}
 
 typedef struct {
     uint64_t length;
     unsigned char chars[1];
 } string;
+
 const string consts[256] = {
-	{1, (char)0}, {1, (char)1}, {1, (char)2},
-	{1, (char)3}, {1, (char)4}, {1, (char)5},
-	{1, (char)6}, {1, (char)7}, {1, (char)8},
-	{1, (char)9}, {1, (char)10}, {1, (char)11},
-	{1, (char)12}, {1, (char)13}, {1, (char)14},
-	{1, (char)15}, {1, (char)16}, {1, (char)17},
-	{1, (char)18}, {1, (char)19}, {1, (char)20},
-	{1, (char)21}, {1, (char)22}, {1, (char)23},
-	{1, (char)24}, {1, (char)25}, {1, (char)26},
-	{1, (char)27}, {1, (char)28}, {1, (char)29},
-	{1, (char)30}, {1, (char)31}, {1, (char)32},
-	{1, (char)33}, {1, (char)34}, {1, (char)35},
-	{1, (char)36}, {1, (char)37}, {1, (char)38},
-	{1, (char)39}, {1, (char)40}, {1, (char)41},
-	{1, (char)42}, {1, (char)43}, {1, (char)44},
-	{1, (char)45}, {1, (char)46}, {1, (char)47},
-	{1, (char)48}, {1, (char)49}, {1, (char)50},
-	{1, (char)51}, {1, (char)52}, {1, (char)53},
-	{1, (char)54}, {1, (char)55}, {1, (char)56},
-	{1, (char)57}, {1, (char)58}, {1, (char)59},
-	{1, (char)60}, {1, (char)61}, {1, (char)62},
-	{1, (char)63}, {1, (char)64}, {1, (char)65},
-	{1, (char)66}, {1, (char)67}, {1, (char)68},
-	{1, (char)69}, {1, (char)70}, {1, (char)71},
-	{1, (char)72}, {1, (char)73}, {1, (char)74},
-	{1, (char)75}, {1, (char)76}, {1, (char)77},
-	{1, (char)78}, {1, (char)79}, {1, (char)80},
-	{1, (char)81}, {1, (char)82}, {1, (char)83},
-	{1, (char)84}, {1, (char)85}, {1, (char)86},
-	{1, (char)87}, {1, (char)88}, {1, (char)89},
-	{1, (char)90}, {1, (char)91}, {1, (char)92},
-	{1, (char)93}, {1, (char)94}, {1, (char)95},
-	{1, (char)96}, {1, (char)97}, {1, (char)98},
-	{1, (char)99}, {1, (char)100}, {1, (char)101},
-	{1, (char)102}, {1, (char)103}, {1, (char)104},
-	{1, (char)105}, {1, (char)106}, {1, (char)107},
-	{1, (char)108}, {1, (char)109}, {1, (char)110},
-	{1, (char)111}, {1, (char)112}, {1, (char)113},
-	{1, (char)114}, {1, (char)115}, {1, (char)116},
-	{1, (char)117}, {1, (char)118}, {1, (char)119},
-	{1, (char)120}, {1, (char)121}, {1, (char)122},
-	{1, (char)123}, {1, (char)124}, {1, (char)125},
-	{1, (char)126}, {1, (char)127}, {1, (char)128},
-	{1, (char)129}, {1, (char)130}, {1, (char)131},
-	{1, (char)132}, {1, (char)133}, {1, (char)134},
-	{1, (char)135}, {1, (char)136}, {1, (char)137},
-	{1, (char)138}, {1, (char)139}, {1, (char)140},
-	{1, (char)141}, {1, (char)142}, {1, (char)143},
-	{1, (char)144}, {1, (char)145}, {1, (char)146},
-	{1, (char)147}, {1, (char)148}, {1, (char)149},
-	{1, (char)150}, {1, (char)151}, {1, (char)152},
-	{1, (char)153}, {1, (char)154}, {1, (char)155},
-	{1, (char)156}, {1, (char)157}, {1, (char)158},
-	{1, (char)159}, {1, (char)160}, {1, (char)161},
-	{1, (char)162}, {1, (char)163}, {1, (char)164},
-	{1, (char)165}, {1, (char)166}, {1, (char)167},
-	{1, (char)168}, {1, (char)169}, {1, (char)170},
-	{1, (char)171}, {1, (char)172}, {1, (char)173},
-	{1, (char)174}, {1, (char)175}, {1, (char)176},
-	{1, (char)177}, {1, (char)178}, {1, (char)179},
-	{1, (char)180}, {1, (char)181}, {1, (char)182},
-	{1, (char)183}, {1, (char)184}, {1, (char)185},
-	{1, (char)186}, {1, (char)187}, {1, (char)188},
-	{1, (char)189}, {1, (char)190}, {1, (char)191},
-	{1, (char)192}, {1, (char)193}, {1, (char)194},
-	{1, (char)195}, {1, (char)196}, {1, (char)197},
-	{1, (char)198}, {1, (char)199}, {1, (char)200},
-	{1, (char)201}, {1, (char)202}, {1, (char)203},
-	{1, (char)204}, {1, (char)205}, {1, (char)206},
-	{1, (char)207}, {1, (char)208}, {1, (char)209},
-	{1, (char)210}, {1, (char)211}, {1, (char)212},
-	{1, (char)213}, {1, (char)214}, {1, (char)215},
-	{1, (char)216}, {1, (char)217}, {1, (char)218},
-	{1, (char)219}, {1, (char)220}, {1, (char)221},
-	{1, (char)222}, {1, (char)223}, {1, (char)224},
-	{1, (char)225}, {1, (char)226}, {1, (char)227},
-	{1, (char)228}, {1, (char)229}, {1, (char)230},
-	{1, (char)231}, {1, (char)232}, {1, (char)233},
-	{1, (char)234}, {1, (char)235}, {1, (char)236},
-	{1, (char)237}, {1, (char)238}, {1, (char)239},
-	{1, (char)240}, {1, (char)241}, {1, (char)242},
-	{1, (char)243}, {1, (char)244}, {1, (char)245},
-	{1, (char)246}, {1, (char)247}, {1, (char)248},
-	{1, (char)249}, {1, (char)250}, {1, (char)251},
-	{1, (char)252}, {1, (char)253}, {1, (char)254},
-	{1, (char)255}
+        {1, {0}}, {1, {1}}, {1, {2}}, {1, {3}}, {1, {4}}, {1, {5}},
+        {1, {6}}, {1, {7}}, {1, {8}}, {1, {9}}, {1, {10}}, {1, {11}},
+        {1, {12}}, {1, {13}}, {1, {14}}, {1, {15}}, {1, {16}}, {1, {17}},
+        {1, {18}}, {1, {19}}, {1, {20}}, {1, {21}}, {1, {22}}, {1, {23}},
+        {1, {24}}, {1, {25}}, {1, {26}}, {1, {27}}, {1, {28}}, {1, {29}},
+        {1, {30}}, {1, {31}}, {1, {32}}, {1, {33}}, {1, {34}}, {1, {35}},
+        {1, {36}}, {1, {37}}, {1, {38}}, {1, {39}}, {1, {40}}, {1, {41}},
+        {1, {42}}, {1, {43}}, {1, {44}}, {1, {45}}, {1, {46}}, {1, {47}},
+        {1, {48}}, {1, {49}}, {1, {50}}, {1, {51}}, {1, {52}}, {1, {53}},
+        {1, {54}}, {1, {55}}, {1, {56}}, {1, {57}}, {1, {58}}, {1, {59}},
+        {1, {60}}, {1, {61}}, {1, {62}}, {1, {63}}, {1, {64}}, {1, {65}},
+        {1, {66}}, {1, {67}}, {1, {68}}, {1, {69}}, {1, {70}}, {1, {71}},
+        {1, {72}}, {1, {73}}, {1, {74}}, {1, {75}}, {1, {76}}, {1, {77}},
+        {1, {78}}, {1, {79}}, {1, {80}}, {1, {81}}, {1, {82}}, {1, {83}},
+        {1, {84}}, {1, {85}}, {1, {86}}, {1, {87}}, {1, {88}}, {1, {89}},
+        {1, {90}}, {1, {91}}, {1, {92}}, {1, {93}}, {1, {94}}, {1, {95}},
+        {1, {96}}, {1, {97}}, {1, {98}}, {1, {99}}, {1, {100}}, {1, {101}},
+        {1, {102}}, {1, {103}}, {1, {104}}, {1, {105}}, {1, {106}}, {1, {107}},
+        {1, {108}}, {1, {109}}, {1, {110}}, {1, {111}}, {1, {112}}, {1, {113}},
+        {1, {114}}, {1, {115}}, {1, {116}}, {1, {117}}, {1, {118}}, {1, {119}},
+        {1, {120}}, {1, {121}}, {1, {122}}, {1, {123}}, {1, {124}}, {1, {125}},
+        {1, {126}}, {1, {127}}, {1, {128}}, {1, {129}}, {1, {130}}, {1, {131}},
+        {1, {132}}, {1, {133}}, {1, {134}}, {1, {135}}, {1, {136}}, {1, {137}},
+        {1, {138}}, {1, {139}}, {1, {140}}, {1, {141}}, {1, {142}}, {1, {143}},
+        {1, {144}}, {1, {145}}, {1, {146}}, {1, {147}}, {1, {148}}, {1, {149}},
+        {1, {150}}, {1, {151}}, {1, {152}}, {1, {153}}, {1, {154}}, {1, {155}},
+        {1, {156}}, {1, {157}}, {1, {158}}, {1, {159}}, {1, {160}}, {1, {161}},
+        {1, {162}}, {1, {163}}, {1, {164}}, {1, {165}}, {1, {166}}, {1, {167}},
+        {1, {168}}, {1, {169}}, {1, {170}}, {1, {171}}, {1, {172}}, {1, {173}},
+        {1, {174}}, {1, {175}}, {1, {176}}, {1, {177}}, {1, {178}}, {1, {179}},
+        {1, {180}}, {1, {181}}, {1, {182}}, {1, {183}}, {1, {184}}, {1, {185}},
+        {1, {186}}, {1, {187}}, {1, {188}}, {1, {189}}, {1, {190}}, {1, {191}},
+        {1, {192}}, {1, {193}}, {1, {194}}, {1, {195}}, {1, {196}}, {1, {197}},
+        {1, {198}}, {1, {199}}, {1, {200}}, {1, {201}}, {1, {202}}, {1, {203}},
+        {1, {204}}, {1, {205}}, {1, {206}}, {1, {207}}, {1, {208}}, {1, {209}},
+        {1, {210}}, {1, {211}}, {1, {212}}, {1, {213}}, {1, {214}}, {1, {215}},
+        {1, {216}}, {1, {217}}, {1, {218}}, {1, {219}}, {1, {220}}, {1, {221}},
+        {1, {222}}, {1, {223}}, {1, {224}}, {1, {225}}, {1, {226}}, {1, {227}},
+        {1, {228}}, {1, {229}}, {1, {230}}, {1, {231}}, {1, {232}}, {1, {233}},
+        {1, {234}}, {1, {235}}, {1, {236}}, {1, {237}}, {1, {238}}, {1, {239}},
+        {1, {240}}, {1, {241}}, {1, {242}}, {1, {243}}, {1, {244}}, {1, {245}},
+        {1, {246}}, {1, {247}}, {1, {248}}, {1, {249}}, {1, {250}}, {1, {251}},
+        {1, {252}}, {1, {253}}, {1, {254}}, {1, {255}}
 };
+
 string empty = {0, ""};
 
-uint64_t *_initArray(uint64_t size, uint64_t init)
-{
-    int i;
-    uint64_t *a = malloc((size + 1) * sizeof(*a));
-	a[0] = size;
-    for (i = 1; i <= size; i++)
-		a[i] = init;
-    return a+1;
+#define fail_and_exit(...) do { \
+    fprintf(stderr, "Fatal: " __VA_ARGS__); \
+    _Exit(EXIT_FAILURE); \
+} while(0)
+
+/**
+ * Allocate and pre-fill an array of integers with an initial value.
+ * The array is allocated in such a way so that a[-1] holds the array size.
+ *
+ * @param size The amount of integers the array will hold
+ * @param init The initial value to fill the array with
+ * @return A pointer to the allocated array. a[-1] will hold the array size.
+ */
+int64_t *_initArray(uint64_t size, int64_t init) {
+    int64_t *a = malloc((size + 1) * sizeof(*a));
+    if (!a)
+        fail_and_exit("Problem allocating memory for array\n");
+
+    a[0] = size;
+    for (size_t i = 1; i <= size; i++)
+        a[i] = init;
+
+    return (a + 1);
 }
-void _checkIndexArray(uint64_t *a, uint64_t i)
-{
-	if(i<0 || i>=a[-1]) {
-		fprintf(stderr, "indice %ld excedido!\n", i);
-		_Exit(-1);
-	}
+
+/**
+ * Check array index for potential out of bounds condition. This function
+ * will exit the program if the index is out of bounds.
+ *
+ * @param arr Array where the access is to be performed
+ * @param i Desired index to be accessed
+ */
+void _checkIndexArray(int64_t *arr, int64_t i) {
+    if (i < 0 || i >= arr[-1])
+        fail_and_exit("Index %" PRId64 " out of bounds\n", i);
 }
-uint64_t *_allocRecord(uint64_t ctos, ...)
-{
-    int i;
-    uint64_t *p, *a;
-	va_list va;
-    p = a = malloc(ctos * sizeof(*p));
-	va_start(va, ctos);
-    for (i = 0; i < ctos; i ++)
-		*p++ = va_arg(va, uint64_t);
-    return a;
+
+/**
+ * Allocate and pre-fill a record with the arguments provided
+ *
+ * @param ctos How many arguments are provided
+ * @param ... The arguments
+ * @return A pointer to the allocated record
+ */
+int64_t *_allocRecord(uint64_t qty, ...) {
+    va_list va;
+    int64_t *r = malloc(qty * sizeof(*r));
+    if (!r)
+        fail_and_exit("Problem allocating memory for record\n");
+
+    va_start(va, qty);
+    for (size_t i = 0; i < qty; i++)
+        r[i] = va_arg(va, int64_t);
+    va_end(va);
+
+    return r;
 }
-void _checkNil(uint64_t* r)
-{
-	if(r==0) {
-		fprintf(stderr, "Nil!\n");
-		_Exit(-1);
-	}
+
+/**
+ * Ensure a pointer to a record is not null. This function will exit
+ * the program if this is the case.
+ *
+ * @param r A possible pointer to a record.
+ */
+void _checkNil(uint64_t *r) {
+    if (!r)
+        fail_and_exit("Nil record encountered\n");
 }
-uint64_t _stringCompare(string *s, string *t)
-{
-    int i;
-    if(s == t)
-		return 0;
-    for(i = 0; i<s->length && i<t->length; i++)
-		if(s->chars[i]!=t->chars[i])
-		    return s->chars[i]-t->chars[i];
-	return s->length-t->length;
+
+/**
+ * Compare two strings, in the style of strcmp
+ *
+ * @param s The first string
+ * @param t The second string
+ * @return A negative, zero or positive number
+ */
+int64_t _stringCompare(string *s, string *t) {
+    if (s == t)
+        return 0;
+
+    for (size_t i = 0; i < s->length && i < t->length; i++) {
+        if (s->chars[i] != t->chars[i]) {
+            return (int64_t) s->chars[i] - (int64_t) t->chars[i];
+        }
+    }
+
+    return (int64_t) s->length - (int64_t) t->length;
 }
-void print(string *s)
-{
-    int i;
+
+/**
+ * Print a string to standard output
+ *
+ * @param s The string to print to standard output
+ */
+void print(string *s) {
     unsigned char *p = s->chars;
-    for (i = 0; i < s->length; i++, p++)
-		putchar(*p);
+    for (size_t i = 0; i < s->length; i++, p++)
+        putchar(*p);
 }
-void flush()
-{
+
+/**
+ * Print an integer to standard output. The number is prepended with
+ * the string "INT: " and a newline is printed after it. This function
+ * is primarily meant to aid debugging.
+ *
+ * @param x Number to print
+ */
+void printInt(int64_t x) {
+    printf("INT: %" PRId64 "\n", x);
+}
+
+/**
+ * Flush the standard output buffer
+ */
+void flush() {
     fflush(stdout);
 }
-uint64_t ord(string *s)
-{
+
+
+/**
+ * Converts the first character of a string into its ASCII codepoint
+ * equivalent. If the provided string is empty, it returns -1.
+ *
+ * @param s The string to inspect
+ * @return An ASCII code or -1 if an error occured
+ */
+int64_t ord(string *s) {
     if (s->length == 0)
-		return -1;
-    else
-		return s->chars[0];
+        return -1;
+
+    return s->chars[0];
 }
-string *chr(uint64_t i)
-{
-    if (i < 0 || i >= 256) {
-		printf("chr(%ld) out of range\n", i);
-		_Exit(1);
-    }
-    return (string*)(consts + i);
+
+/**
+ * Converts an ASCII codepoint into its equivalent string of length 1.
+ * If the codepoint provided is invalid, the program will exit.
+ *
+ * @param i The ASCII codepoint
+ * @return
+ */
+const string *chr(int64_t i) {
+    if (i < 0 || i >= 256)
+        fail_and_exit("chr(%" PRId64 ") out of range\n", i);
+
+    return &consts[i];
 }
-uint64_t size(string *s)
-{
+
+/**
+ * Queries the length of a string
+ * @param s The string in question
+ * @return The length of the string s
+ */
+uint64_t size(string *s) {
     return s->length;
 }
-string *substring(string *s, uint64_t first, uint64_t n)
-{
-    if (first < 0 || first + n > s->length) {
-		printf("substring([%ld],%ld,%ld) out of range\n", s->length, first, n);
-		_Exit(1);
-    }
+
+/**
+ * Returns a new string, which is the substring of s that starts on first
+ * and has length n
+ *
+ * @param s The original string
+ * @param first The first character of the substring
+ * @param n The length of the new string
+ * @return The new string
+ */
+const string *substring(string *s, int64_t first, int64_t n) {
+    if (first < 0 || (uint64_t) first + n > s->length)
+        fail_and_exit("substring([%" PRIu64 "], %" PRId64 ", %" PRId64 ") out of range\n", s->length, first, n);
+
     if (n == 1)
-		return (string*)(consts + s->chars[first]);
-    {
-		string *t = malloc(sizeof(uint64_t) + n);
-		int i;
-		t->length = n;
-		for (i = 0; i < n; i++)
-		    t->chars[i] = s->chars[first + i];
-		return t;
-    }
+        return &consts[s->chars[first]];
+
+    string *t = malloc(sizeof(uint64_t) + n);
+    t->length = n;
+
+    for (ssize_t i = 0; i < n; i++)
+        t->chars[i] = s->chars[first + i];
+
+    return t;
 }
-string *concat(string *a, string *b)
-{
+
+/**
+ * Concatenates two strings into a new string
+ *
+ * @param a The first string
+ * @param b The second string
+ * @return The new string, which is the concatenation of a and b
+ */
+const string *concat(const string *a, const string *b) {
     if (a->length == 0)
-		return b;
-    else if (b->length == 0)
-		return a;
-    else {
-		int i, n = a->length + b->length;
-		string *t = malloc(sizeof(uint64_t) + n);
-		t->length = n;
-		for (i = 0; i < a->length; i++)
-		    t->chars[i] = a->chars[i];
-		for (i = 0; i < b->length; i++)
-		    t->chars[i + a->length] = b->chars[i];
-		return t;
-    }
+        return b;
+
+    if (b->length == 0)
+        return a;
+
+    uint64_t n = a->length + b->length;
+    string *t = malloc(sizeof(uint64_t) + n);
+    t->length = n;
+
+    for (size_t i = 0; i < a->length; i++)
+        t->chars[i] = a->chars[i];
+
+    for (size_t i = 0; i < b->length; i++)
+        t->chars[i + a->length] = b->chars[i];
+
+    return t;
 }
-uint64_t not(uint64_t i)
-{
+
+/**
+ * Returns 1 if the argument is zero, and 0 if the argument is non-zero
+ */
+uint64_t not(uint64_t i) {
     return !i;
 }
-string *getstr()
-{
+
+/**
+ * Reads a character from standard input
+ *
+ * @return A string of length one with the character, or the empty
+ * string if an EOF condition occured.
+ */
+const string *getstr() {
     int i = getc(stdin);
     if (i == EOF)
-		return &empty;
-    else
-		return (string*)(consts + i);
+        return &empty;
+
+    return &consts[i];
 }
 
 uint64_t _tigermain(uint64_t);
 
-int main()
-{
-    int i;
-	/*
-    for (i = 0; i < 256; i++) {
-		consts[i].length = 1;
-		consts[i].chars[0] = i;
-    }
-	*/
-    i = _tigermain(0 /* static link!? */ );
+int main() {
+    uint64_t i = _tigermain(0 /* dummy static link */);
     _Exit(i);
-    return EXIT_FAILURE;
+    return EXIT_FAILURE; /* should never run */
 }
